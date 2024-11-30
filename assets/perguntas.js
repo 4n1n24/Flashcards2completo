d476d7743162246a93783b7fd4efade4e8bf9e53
4n1n24 <00001102212349sp@al.educacao.sp.gov.br>
2024-11-29 22:20:51 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cinema',
+    'Qual é a definição de cinematografia??',
+    'o conjunto de princípios, processos e técnicas utilizados para captar e projetar numa tela imagens estáticas sequenciais (fotogramas) obtidas com uma câmera especial, dando impressão ao espectador de estarem em movimento.'
+)
+
+criaCartao(
+    'História',
+    'Qual o nome das pinturas feitas pelos homens das cavernas?',
+    'Arte Rupestre'
+)
+
+criaCartao(
+    'Cinema',
+    'O que é Audiovisual?',
+    'meio de comunicação que combina elementos visuais e sonoros, ou seja, que pode ser visto e ouvido ao mesmo tempo.'
+)
+
+criaCartao(
+    'Lingua Inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
---
+++ readme.md
@@ -0,0 +1,17 @@
+## Cores
+
+```css
+:root {
+    --text-color: #FA987D;
+    --card-front-color: ##F2C0AF;
+    --card-back-color: #F2835E;
+}
+```
+
+> Para mais cores
+
+- https://colorhunt.co/
+- https://color.adobe.com/pt/create/color-wheel
+
+## Para fontes
+https://fonts.google.com/
